8dbd1fb94729dd8e2ed29b3055cff73c106948d1
Fernando2987 <fernandopalacios51@gmail.com>
2024-09-02 23:13:23 -0300

    Add: Clase Persona y metodos que se necesitab

---
+++ TP3_GRUPO_1/src/ejercicio1/Persona.java
@@ -0,0 +1,69 @@
+package ejercicio1;
+
+
+public class Persona implements  Comparable<Persona> {
+	
+	    private String nombre;
+	    private String apellido;
+	    private String dni;
+
+	    public Persona(String nombre, String apellido, String dni) {
+	        this.nombre = nombre;
+	        this.apellido = apellido;
+	        this.dni = dni;
+	    }
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getApellido() {
+			return apellido;
+		}
+
+		public void setApellido(String apellido) {
+			this.apellido = apellido;
+		}
+
+		public String getDni() {
+			return dni;
+		}
+
+		public void setDni(String dni) {
+			this.dni = dni;
+		}
+	    
+		
+		//Ordena personas alfabéticamente por apellido.
+	 
+		 @Override
+		    public int compareTo(Persona otraPersona) {
+		        return this.apellido.compareTo(otraPersona.apellido);
+		    }
+		 	 
+		   
+		 //Compara dos personas basándose en el DNI.
+		    @Override
+		    public boolean equals(Object obj) {
+		        if (this == obj) return true;
+		        if (obj == null || getClass() != obj.getClass()) return false;
+		        Persona persona = (Persona) obj;
+		        return dni.equals(persona.dni);
+		    }
+		    
+		    //Genera un código hash basado en el DNI.
+		    @Override
+		    public int hashCode() {
+		        return dni.hashCode();
+		    }
+
+		   
+		    @Override
+		    public String toString() {
+		        return nombre + " " + apellido + " - DNI: " + dni;
+		    }
+}
